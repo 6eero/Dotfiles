bb59bb425a17f9b1efc661317b3065de913fed78
6eero <gerotto.alessandro08@gmail.com>
2024-06-19 11:43:30 +0200

    changed rofi theme

--- Scripts/rofi-kill-window.sh
+++ Scripts/rofi-kill-window.sh
@@ -2,7 +2,7 @@
 
 # Power menu script using tofi
 
-CHOSEN=$(printf "Exit\nBack" | rofi -dmenu -i -theme /home/gero/Dotfiles/.config/rofi/powermenu-theme.rasi)
+CHOSEN=$(printf "Exit\nBack" | rofi -dmenu -i -theme $HOME/Dotfiles/.config/rofi/kill-focused-theme.rasi)
 
 case "$CHOSEN" in
 	"Exit") focused_window=$(xdotool getwindowfocus) && xkill -id $focused_window ;;
